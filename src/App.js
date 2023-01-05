6999447fd5fbb7d6dda32aa291c1ee1471af487c
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2023-01-05 14:02:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//instead of the TOP project, I'm going to make my a text based version of the classic name all fifty states game
 
 export default App;
