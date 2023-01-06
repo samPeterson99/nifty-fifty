562998b422c718c118b259bb8790a6820cb20d63
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2023-01-06 12:55:57 -0500

    working game elements( no logic or styling)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
-
+import React, {  useState, useRef } from "react";
 import './App.css';
 
 //instead of the TOP project, I'm going to make my a text based version of the classic name all fifty states game
+
+const App = () => {
+  //create component that plays the game
+  // create shame box
+  //create landing graphic and instructions
+  const input = useRef('')
+  const [found, setFound] = useState([]);
+  const [shame, setShame] = useState([]);
+
+  const arrayOfStateNames = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+
+  const handlePlayerInput = (input) => {
+    let beautifiedInput = beautifyString(input)
+    if (arrayOfStateNames.indexOf(beautifiedInput) < 0) {
+        setShame([...shame, input]);
+      } else if (arrayOfStateNames.indexOf(beautifiedInput) >= 0 && found.indexOf(beautifiedInput) >= 0) {
+        //shame the user
+      } else {
+        setFound([...found, beautifiedInput])
+      }
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handlePlayerInput(e.target.value);
+      e.target.value = ''
+    }
+  }
+
+  const beautifyString = (string) => {
+    let prettyString = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+    return prettyString
+  }
+
+  const foundItems = found.sort().map((state) =>
+      <li>{state}</li>
+  );
+
+  const shameBoxContents = shame.map((disgrace) => 
+      <li>{disgrace}</li>
+  )
+
+
+
+
+  return (
+    <div>
+      <input type='text' ref={input} onKeyDown={handleKeyPress}></input>
+      <ol>{foundItems}</ol>
+      <ul>
+        <h3>Shame Box</h3>
+        {shameBoxContents}
+      </ul>
+    </div>
+  )
+
+}
 
 export default App;
---
+++ src/Input-and-Score.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+const InputAndScore = () => {
+    //this needs to hold the logic for the variable found , but the routing is done in app
+}
+
+export default InputAndScore;
