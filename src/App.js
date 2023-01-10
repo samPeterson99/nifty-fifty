97c83b370b15fede318a1889c74ea7093a8026df
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2023-01-09 21:17:29 -0500

    start page, win page, and win logic coded

--- src/App.js
+++ src/App.js
@@ -1,72 +1,29 @@
 import React, {  useState, useRef } from "react";
+import PlayTheGame from "./Input-and-Score";
 import './App.css';
 
 //instead of the TOP project, I'm going to make my a text based version of the classic name all fifty states game
-
+//add winning logic
+//style landing page
+//style winning page
 const App = () => {
-  //create win logic
-  //fix state table
-  //create landing graphic and instructions
-  const input = useRef('')
-  const [found, setFound] = useState([]);
-  const [shame, setShame] = useState([]);
+  const [pageState, setPageState] = useState('startPage')
 
-  const arrayOfStateNames = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-
-
-  const handlePlayerInput = (input) => {
-    let beautifiedInput = beautifyString(input)
-    if (arrayOfStateNames.indexOf(beautifiedInput) < 0) {
-        setShame([...shame, input]);
-      } else if (arrayOfStateNames.indexOf(beautifiedInput) >= 0 && found.indexOf(beautifiedInput) >= 0) {
-        //shame the user
-      } else {
-        setFound([...found, beautifiedInput])
-      }
+  const startPageClick = () => {
+    setPageState('playTheGame')
   }
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handlePlayerInput(e.target.value);
-      e.target.value = ''
-    }
-  }
-
-  const beautifyString = (string) => {
-    let stringArray = string.split(' ');
-    let prettyArray = stringArray.map(word =>
-      word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    let prettyString = prettyArray.join(' ')
-    return prettyString
-  }
-
-
-  let finds = 0;
-  let shames = 0;
-  const foundItems = found.sort().map((state) =>
-      <li key= {finds++}>{state}</li>
-  );
-
-  const shameBoxContents = shame.map((disgrace) => 
-      <li key={shames++}>{disgrace}</li>
-  )
-
-
-
-
-  return (
-    <div id="appBox">
-      <input type='text' id="inputBox" ref={input} onKeyDown={handleKeyPress}></input>
-      <ol id="foundItems">{foundItems}</ol>
-      <div id="shameBox">
-        <h3 id="shameBoxHead">Shame Box</h3>
-          <ul>
-            {shameBoxContents}
-          </ul>
-          <h5 id="shameBoxFoot">*Ignore if not from the United States</h5>
+    if (pageState === 'startPage') {
+      return (
+      <div id="startPage" onClick={startPageClick}>
+        <h1 id="startTitle">Fifty</h1>
+        <h5 id="startInstructions">click anywhere to begin</h5>
+        <h1 id="startTitle">Nifty</h1>
       </div>
-    </div>
-  )
+      )
+    } else {
+      return <PlayTheGame />
+    }
 
 }
 
--- src/Input-and-Score.js
+++ src/Input-and-Score.js
@@ -1,7 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-const InputAndScore = () => {
-    //this needs to hold the logic for the variable found , but the routing is done in app
+const PlayTheGame = () => {
+    const input = useRef('')
+  const [found, setFound] = useState([]);
+  const [shame, setShame] = useState([]);
+
+  const arrayOfStateNames = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+
+  const handlePlayerInput = (input) => {
+    let beautifiedInput = beautifyString(input)
+    if (arrayOfStateNames.indexOf(beautifiedInput) < 0) {
+        setShame([...shame, input]);
+      } else if (arrayOfStateNames.indexOf(beautifiedInput) >= 0 && found.indexOf(beautifiedInput) >= 0) {
+        //shame the user
+      } else {
+        setFound([...found, beautifiedInput])
+      }
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handlePlayerInput(e.target.value);
+      e.target.value = ''
+    }
+  }
+
+  const beautifyString = (string) => {
+    let stringArray = string.split(' ');
+    let prettyArray = stringArray.map(word =>
+      word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    let prettyString = prettyArray.join(' ')
+    return prettyString
+  }
+
+
+  let finds = 0;
+  let shames = 0;
+  const foundItems = found.sort().map((state) =>
+      <li key= {finds++}>{state}</li>
+  );
+
+  const shameBoxContents = shame.map((disgrace) => 
+      <li className="shame" key={shames++}>{disgrace}</li>
+  )
+
+
+  if (arrayOfStateNames.length === 50) {
+    return (
+    <div id="winScreenBox" >
+      <h1 id="winnerText">you</h1>
+      <h5 id="winnerInstructions">refresh page to play again</h5>
+      <h1 id="winnerText">won</h1>
+    </div>
+    )
+  } else 
+
+  return (
+
+    <div id="appBox">
+        <div id="topRow">
+            <h1 id="title">Fifty-Nifty</h1>
+            <div id="shameBox">
+                <h3 id="shameBoxHead">Shame Box</h3>
+                <ul id="shameItems">
+                    {shameBoxContents}
+                </ul>
+                <h5 id="shameBoxFoot">*Ignore if not from the United States</h5>
+            </div>
+        </div>
+        <input type='text' id="inputBox" ref={input} onKeyDown={handleKeyPress}></input>
+        <ol id="foundItems">{foundItems}</ol>
+      
+
+    </div>
+  )
 }
 
-export default InputAndScore;
+export default PlayTheGame;
